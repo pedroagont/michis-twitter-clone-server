b489d356e874db86df73f3e10e735bd8649ff5a3
Pedro Gonzalez <pgonzalez@post.com>
2020-07-20 03:36:15 -0500

    Agregar conexión con firebase, proteger llaves privadas, async/await

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+const dotenv = require('dotenv');
+dotenv.config();
 const express = require('express');
 const cors = require('cors');
 const app = express();
@@ -6,6 +8,16 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
+const admin = require('firebase-admin');
+const serviceAccount = require(`./${process.env.FIREBASE_JSON}`);
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+  databaseURL: "https://michis-michis.firebaseio.com"
+});
+
+const db = admin.firestore();
+
 app.get('/', (req, res) => res.send('Hello World!'));
 
 const isValidMiau= (miau) => {
@@ -13,19 +25,22 @@ const isValidMiau= (miau) => {
          miau.content && miau.content.toString().trim() !== '';
 }
 
-app.post('/miaus', (req, res) => {
+app.post('/miaus', async (req, res) => {
   if(isValidMiau(req.body)) {
     const { name, content } = req.body
     const miau = {
       name: name.toString(),
       content: content.toString(),
     }
-    console.log(miau);
-    res.status(201).json({ miau, message: 'Buen Michi' });
+    await db.collection('miaus').doc().set(miau)
+      .then(response => {
+        console.log(response);
+        res.status(201).json({ response, miau, message: 'Buen Michi :)' });
+      })
+      .catch(error => console.log(error));
   } else {
     console.log('Mal michi :(');
-    res.status(422);
-    res.json({ message: 'Un michi tiene que tener nombre y miau' });
+    res.status(422).json({ message: 'Un michi tiene que tener nombre y miau' });
   }
 });
 
