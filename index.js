77ca9e90af904ef6a8e3f8c415d14b69f43ae3ab
Pedro Gonzalez <pgonzalez@post.com>
2020-07-20 14:23:17 -0500

    Corregir private keys de firebase

--- index.js
+++ index.js
@@ -15,7 +15,14 @@ app.use(rateLimit({
 }));
 
 const admin = require('firebase-admin');
-const serviceAccount = require(`./${process.env.FIREBASE_JSON}`);
+const serviceAccount = {
+  "project_id": process.env.FIREBASE_PROJECT_ID,
+  "private_key_id": process.env.FIREBASE_PRIVATE_KEY_ID,
+  "private_key": process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'),
+  "client_email": process.env.FIREBASE_CLIENT_EMAIL,
+  "client_id": process.env.FIREBASE_CLIENT_ID,
+}
+
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
